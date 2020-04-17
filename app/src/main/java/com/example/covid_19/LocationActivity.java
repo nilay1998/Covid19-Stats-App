a7e252b2895f614129444abf68f77ef1c00cbfb9
Nilay Gupta <nilaygupta1998@gmail.com>
2020-04-18 03:16:52 +0530

    showing infectedlocations

--- app/src/main/java/com/example/covid_19/LocationActivity.java
+++ app/src/main/java/com/example/covid_19/LocationActivity.java
@@ -1,9 +1,5 @@
 package com.example.covid_19;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -16,32 +12,53 @@ import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.covid_19.Adapters.InfectedLocationAdapter;
+import com.example.covid_19.Service.Models.InfectedLocationModel;
+import com.example.covid_19.ViewModels.LocationActivityViewModel;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LocationActivity extends AppCompatActivity {
 
+    @BindView(R.id.infectedLocation_recyclerView)
+    RecyclerView infectedLocationRecyclerView;
     private FusedLocationProviderClient mFusedLocationClient;
     private int PERMISSION_ID = 44;
-    private String latitude;
-    private String longitude;
     private static final String TAG = "LocationActivity";
+    private LocationActivityViewModel mLocationActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        ButterKnife.bind(this);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
 
+        mLocationActivityViewModel = new ViewModelProvider(this).get(LocationActivityViewModel.class);
+        mLocationActivityViewModel.init();
+        mLocationActivityViewModel.getInfectedLocation().observe(this, new Observer<InfectedLocationModel>() {
+            @Override
+            public void onChanged(InfectedLocationModel infectedLocationModel) {
+                mLocationActivityViewModel.setmInfectedLocationList();
+                getLastLocation();
+            }
+        });
     }
 
     @SuppressLint("MissingPermission")
-    private void getLastLocation(){
+    private void getLastLocation() {
         if (checkPermissions()) {
             if (isLocationEnabled()) {
                 mFusedLocationClient.getLastLocation()
@@ -49,13 +66,13 @@ public class LocationActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(Location location) {
                                 if (location != null) {
-                                    latitude=location.getLatitude()+"";
-                                    longitude=location.getLongitude()+"";
+                                    Log.e(TAG, "onSuccess: LAT"+location.getLatitude());
+                                    Log.e(TAG, "onSuccess: LON"+location.getLongitude());
+                                    inflateRecyclerView(location);
                                 }
                             }
                         });
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivity(intent);
@@ -65,15 +82,21 @@ public class LocationActivity extends AppCompatActivity {
         }
     }
 
-    private boolean checkPermissions(){
+    private void inflateRecyclerView(Location location) {
+        InfectedLocationAdapter adapter = new InfectedLocationAdapter(mLocationActivityViewModel.getNearestLocation(location));
+        infectedLocationRecyclerView.setAdapter(adapter);
+        infectedLocationRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+
+    private boolean checkPermissions() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
         return false;
     }
 
-    private void requestPermissions(){
+    private void requestPermissions() {
         ActivityCompat.requestPermissions(
                 this,
                 new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
@@ -85,13 +108,13 @@ public class LocationActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_ID) {
-            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Granted. Start getting the location information
             }
         }
     }
 
-    private boolean isLocationEnabled(){
+    private boolean isLocationEnabled() {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
                 LocationManager.NETWORK_PROVIDER
--- app/src/main/java/com/example/covid_19/ViewModels/LocationActivityViewModel.java
+++ app/src/main/java/com/example/covid_19/ViewModels/LocationActivityViewModel.java
@@ -1,15 +1,28 @@
 package com.example.covid_19.ViewModels;
 
+import android.location.Location;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.covid_19.Service.Models.InfectedLocationModel;
+import com.example.covid_19.Service.Models.TravelHistoryModel;
 import com.example.covid_19.Service.Repositories.InfectedLocationRepo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
+import static android.content.ContentValues.TAG;
 
 public class LocationActivityViewModel extends ViewModel {
     MutableLiveData<InfectedLocationModel> mInfectedLocation;
     InfectedLocationRepo mRepo;
+    ArrayList<TravelHistoryModel> mInfectedLocationList;
 
     public void init()
     {
@@ -22,5 +35,50 @@ public class LocationActivityViewModel extends ViewModel {
     public LiveData<InfectedLocationModel> getInfectedLocation()
     {
         return mInfectedLocation;
+    }
+
+    public void setmInfectedLocationList()
+    {
+        mInfectedLocationList=new ArrayList<>(Arrays.asList(mInfectedLocation.getValue().getTravel_history()));
+    }
+
+    public ArrayList<TravelHistoryModel> getNearestLocation(Location location)
+    {
+        ArrayList<TravelHistoryModel> arrayList=new ArrayList<>();
+        for(int i=0;i<mInfectedLocationList.size();i++)
+        {
+            TravelHistoryModel travelHistoryModel=mInfectedLocationList.get(i);
+            if(travelHistoryModel.getLatlong()!=null && !travelHistoryModel.getLatlong().isEmpty())
+            {
+                String arr[]=travelHistoryModel.getLatlong().split(",");
+                Double distance=getDifference(location.getLatitude(),location.getLongitude(),Double.parseDouble(arr[0].trim()),Double.parseDouble(arr[1].trim()));
+                travelHistoryModel.setLatlong(String.valueOf(distance));
+                arrayList.add(travelHistoryModel);
+            }
+        }
+
+        Collections.sort(arrayList, new Comparator<TravelHistoryModel>()
+        {
+            public int compare(TravelHistoryModel o1, TravelHistoryModel o2) {
+                Double dis=Double.parseDouble(o1.getLatlong())-Double.parseDouble(o2.getLatlong());
+                return dis.intValue();
+            }
+        });
+
+        return arrayList;
+    }
+
+    private double getDifference(double currLat, double currLon,double infLat, double infLon)
+    {
+        int radius=6378000;
+        double x1=radius*Math.cos(Math.toRadians(currLat))*Math.cos(Math.toRadians(currLon));
+        double y1=radius*Math.cos(Math.toRadians(currLat))*Math.sin(Math.toRadians(currLon));
+        double z1=radius*Math.sin(Math.toRadians(currLat));
+
+        double x2=radius*Math.cos(Math.toRadians(infLat))*Math.cos(Math.toRadians(infLon));
+        double y2=radius*Math.cos(Math.toRadians(infLat))*Math.sin(Math.toRadians(infLon));
+        double z2=radius*Math.sin(Math.toRadians(infLat));
+
+        return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2)+Math.pow(z1-z2,2));
     }
 }
